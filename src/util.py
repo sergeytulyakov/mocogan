d59b5b7bf021b0473f20f62659f5c8685fa6cf21
Sergey Tulyakov <stulyakov@snapchat.com>
2017-10-16 18:23:53 -0700

    small fix

--- src/util.py
+++ src/util.py
@@ -1,3 +1,8 @@
+"""
+Copyright (C) 2017 NVIDIA Corporation.  All rights reserved.
+Licensed under the CC BY-NC-ND 4.0 license (https://creativecommons.org/licenses/by-nc-nd/4.0/legalcode).
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 
